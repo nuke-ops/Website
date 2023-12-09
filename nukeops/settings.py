a02bc6bc98c7b07816872cb2abf51469ad724db3
Maksxpl <maks.kozon@gmail.com>
2023-12-09 08:50:37 +0100

    Update settings.py

--- nukeops/settings.py
+++ nukeops/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = conf["secret_key"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "nukeops.com"]
 
 
 # Application definition
