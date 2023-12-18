ff15e26cd10ddb7afbedf5a688e471783ebd1fc8
Maksxpl <maks.kozon@gmail.com>
2023-12-18 07:44:37 +0100

    stream css and typo fix

--- stream/urls.py
+++ stream/urls.py
@@ -3,6 +3,6 @@ from .views import stream_list, stream_create, stream_watch
 
 urlpatterns = [
     path("stream/", stream_list, name="stream_list"),
-    path("stream/create", stream_create, name="stream_create"),
+    path("stream/create/", stream_create, name="stream_create"),
     path("stream/<int:stream_id>/", stream_watch, name="stream_watch"),
 ]
