abd714622759b2fce193b5f34209fb7cbf937b00
Maksxpl <maks.kozon@gmail.com>
2023-12-18 07:22:34 +0100

    stream app structure and player update

--- stream/urls.py
+++ stream/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import stream_list, create_stream, stream_details
+from .views import stream_list, stream_create, stream_watch
 
 urlpatterns = [
     path("stream/", stream_list, name="stream_list"),
-    path("stream/create", create_stream, name="create_stream"),
-    path("stream/<int:stream_id>/", stream_details, name="stream_detail"),
+    path("stream/create", stream_create, name="stream_create"),
+    path("stream/<int:stream_id>/", stream_watch, name="stream_watch"),
 ]
