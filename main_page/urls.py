3dbb22aed56d146b22c3b0cdd325988b40b38d83
Maksxpl <maks.kozon@gmail.com>
2024-01-14 16:56:57 +0100

    ss13 update

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import main, main_ss13, main_ss13_rules
+from .views import main, main_ss13, main_ss13_rules, main_ss13_motd
 
 
 urlpatterns = [
     path("", main, name="main"),
     path("ss13/", main_ss13, name="main_ss13"),
     path("ss13/rules/", main_ss13_rules, name="main_ss13_rules"),
+    path("ss13/MOTD/", main_ss13_motd, name="main_ss13_motd"),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
 def main(request):
@@ -9,5 +10,11 @@ def main_ss13(request):
     return render(request, "main/ss13.html")
 
 
+@xframe_options_exempt
 def main_ss13_rules(request):
     return render(request, "main/rules.html")
+
+
+@xframe_options_exempt
+def main_ss13_motd(request):
+    return render(request, "main/ss13_MOTD.html")
